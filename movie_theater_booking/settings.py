d15f5a5dd5defda5e4fb109702cb0573798a46a1
Vincent Faragalli <vinfaragalli@gmail.com>
2025-10-06 01:16:11 +0000

    deployment render stuff

--- movie_theater_booking/settings.py
+++ movie_theater_booking/settings.py
@@ -74,11 +74,13 @@ WSGI_APPLICATION = 'movie_theater_booking.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+import dj_database_url
+import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     }
 }
 
